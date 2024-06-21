5af0cfdb9d337ca97a462afa7f89bbffc62bd719
chsj9251 <chsj9251@gmail.com>
2024-06-21 11:39:59 +0900

    0621

--- ticket/index.php
+++ ticket/index.php
@@ -8,32 +8,42 @@
 </head>
 <body>
     <div class="container">
-        <h1 class="ticket-title">이용권 관리</h1>
-        <div class="home-button">
-            <a href="/login/main_page.php"><img src="./icon/home.png" alt="메인메뉴"></a>
+    <body>
+    <h1>이용권 생성 테스트</h1>
+    <form method="POST" action="generate_ticket.php">
+        <div class="input-row">
+            <input type="submit" value="QR 생성">
         </div>
-        <div id="ticket-table">
-            <table>
-                <thead>
-                    <tr>
-                        <th>ticket_no</th>
-                        <th>ticket_code</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    include 'functions.php';
-                    
-                    $tickets = getAllTickets();
-                    foreach ($tickets as $ticket) {
-                        echo "<tr>";
-                        echo "<td>" . $ticket['ticket_no'] . "</td>";
-                        echo "<td>" . $ticket['ticket_code'] . "</td>";
-                        echo "</tr>";
-                    }
-                    ?>
-                </tbody>
-            </table>
+    </form>
+    <br>
+    <table>
+        <thead>
+            <tr>
+                <th>ticket_no</th>
+                <th>ticket_code</th>
+                <th>ticket_qr</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            session_start();
+            require '../db_conn.php';
+            $result = $conn->query("SELECT * FROM ticket");
+
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>{$row['ticket_no']}</td>";
+                    echo "<td>{$row['ticket_code']}</td>";
+                    echo "<td><img src='{$row['ticket_qr']}' alt='QR Code'></td>";
+                    echo "</tr>";
+                }
+            }
+            $conn->close();
+            ?>
+        </tbody>
+    </table>
+</body>
         </div>
     </div>
 </body>
